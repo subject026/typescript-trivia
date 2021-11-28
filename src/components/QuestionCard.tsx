ec0df55e3a6bee099831ae461c9ca77cd704d9b8
subject026 <subject026@protonmail.com>
2021-11-28 20:45:22 +0000

    initial question card

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -3,38 +3,42 @@ import React from "react";
 import { Question } from "../App";
 
 type Props = {
-  question?: Question;
-  answers?: string[];
-  callback?: any;
-  userAnswer?: boolean;
+  question: Question;
+  callback: (isCorrect: boolean) => void;
   questionNr?: number;
   totalQuestions?: number;
 };
 
 const QuestionCard: React.FC<Props> = ({
   question,
-  answers,
   callback,
-  userAnswer,
   questionNr,
   totalQuestions,
 }) => {
+  const handleAnswerClick = (answer: string) => {
+    callback(answer === question.correctAnswer);
+  };
+
   return (
-    <div className="p-8 bg-green-200">
+    <div className="p-8 bg-gray-100">
       <p>
         Question: {questionNr} / {totalQuestions}
       </p>
+      <p className="py-6 text-3xl font-bold text-gray-700 italic leading-normal">
+        {question.question}
+      </p>
 
-      {/* <p dangerouslySetInnerHTML={{ __html: question }}></p>
-      <div>
-        {answers.map((answer) => (
-          <div>
-            <button disabled={userAnswer} onClick={callback}>
-              <span dangerouslySetInnerHTML={{ __html: answer }} />
-            </button>
-          </div>
+      <ul className="py-3 text-xl">
+        {question.answers.map((answer, i) => (
+          <li
+            key={i}
+            className="mt-4 p-4 text-center border-4 border-gray-400 cursor-pointer hover:bg-green-400 hover:text-white"
+            onClick={() => handleAnswerClick(answer)}
+          >
+            {answer}
+          </li>
         ))}
-      </div> */}
+      </ul>
     </div>
   );
 };
