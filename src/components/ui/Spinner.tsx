457e2202ae61f909bee483eee0ebdb2b2d44b144
subject026 <subject026@protonmail.com>
2021-11-29 00:20:32 +0000

    change spinner

--- src/components/ui/Spinner.tsx
+++ src/components/ui/Spinner.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Spinner: React.FC = () => (
   <div
     style={{ borderTopColor: "transparent" }}
-    className="w-16 h-16 m-auto mt-48 border-4 border-blue-400 border-solid rounded-full animate-spin"
+    className="w-16 h-16 m-auto mt-48 border-4 border-gray-400 border-solid rounded-full animate-spin"
   ></div>
 );
 
