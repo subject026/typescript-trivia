60abd5bbbe1db830d2a7305dc4f5a68f3377430c
subject026 <subject026@protonmail.com>
2021-11-29 00:22:13 +0000

    header styling

--- src/components/ui/Heading.tsx
+++ src/components/ui/Heading.tsx
@@ -4,6 +4,8 @@ type Props = {
   children: string;
 };
 
-const Heading: React.FC<Props> = ({ children }) => <h1>{children}</h1>;
+const Heading: React.FC<Props> = ({ children }) => (
+  <h1 className="text-8xl text-center font-bold text-blue-900">{children}</h1>
+);
 
 export default Heading;
